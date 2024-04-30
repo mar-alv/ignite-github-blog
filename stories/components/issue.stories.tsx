5c255e15be698bdb8168ae38e8abd4a5484fd7a8
mar-alv <mar.alv.cod@gmail.com>
2024-04-30 19:21:22 -0300

    Issue #6 - issue story

---
+++ stories/components/issue.stories.tsx
@@ -0,0 +1,32 @@
+import { Issue } from '@components'
+import { customRender } from '../stories-utils'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta = {
+  title: 'Issue',
+  component: Issue,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  render: ({...args}) => {
+		return customRender(
+			<Issue issue={args.issue} tabIndex={args.tabIndex} />
+		)
+  }
+} satisfies Meta<typeof Issue>
+
+export default meta
+
+export const Default: StoryObj = {
+  args: {
+		issue: {
+			id: 0,
+			title: 'Issue title',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			createdAt: new Date().toISOString(),
+			creatorNickname: 'Creator'
+		},
+		tabIndex: 0
+  }
+}
--- stories/stories-utils.tsx
+++ stories/stories-utils.tsx
@@ -1,56 +1,31 @@
-/* import { BrowserRouter } from 'react-router-dom'
-import { Coffee, DeliveryAddress, PaymentMethod } from '@interfaces'
-import { CoffeeContext } from '@context'
+import { BrowserRouter } from 'react-router-dom'
+import { Issue } from '@interfaces'
+import { IssuesContext } from '@contexts'
 import { ReactNode } from 'react'
 
 interface CustomValueProps {
-  coffees?: Coffee[]
-  deliveryAddress?: DeliveryAddress
-	hasConfirmedDelivery?: boolean
-  paymentMethod?: PaymentMethod | null
-  addToCart?(coffee: Coffee): void
-  confirmDelivery?(): void
-  choosePaymentMethod?(paymentMethod: PaymentMethod): void
-  decreaseAmountInCart?(coffeeId: number): void
-  increaseAmountInCart?(coffeeId: number): void
-  removeFromCart?(coffeeId: number): void
-  saveDeliveryAddress?(deliveryAddress: DeliveryAddress): void
+  issues?: Issue[]
+  getIssues?: (query?: string) => Promise<void>
 }
 
 const defaultValue = {
-	coffees: [],
-  deliveryAddress: {
-		cep: '',
-		city: '',
-		complement: '',
-		neighborhood: '',
-		number: '',
-		state: '',
-		street: ''
-	},
-	hasConfirmedDelivery: false,
-  paymentMethod: null,
-  addToCart(coffee: Coffee) {},
-  confirmDelivery() {},
-  choosePaymentMethod(paymentMethod: PaymentMethod) {},
-  decreaseAmountInCart(coffeeId: number) {},
-  increaseAmountInCart(coffeeId: number) {},
-  removeFromCart(coffeeId: number) {},
-  saveDeliveryAddress(deliveryAddress: DeliveryAddress) {}
+	issues: [],
+  getIssues(query?: string) {
+		return Promise.resolve()
+	}
 }
 
 export function customRender(children: ReactNode, customValue = {} as CustomValueProps) {
 	return (
     <BrowserRouter>
-			<CoffeeContext.Provider
+			<IssuesContext.Provider
 				value={{
 					...defaultValue,
 					...customValue
 				}}
 				>
 				{children}
-			</CoffeeContext.Provider>
+			</IssuesContext.Provider>
 		</BrowserRouter>
 	)
 }
- */
